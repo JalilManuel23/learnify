f7e4b69900cd98636c3d20519f077d542a443e10
iammarieh <mhf.08@hotmail.com>
2022-10-05 18:16:53 -0500

    Se agrega sección de categorias en homepage

--- public/js/resources_js_components_Home_vue.js
+++ public/js/resources_js_components_Home_vue.js
@@ -13,6 +13,22 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
 /* harmony import */ var _img_bg_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../img/bg.png */ "./resources/img/bg.png");
+/* harmony import */ var _img_desarrollo_ingenieria_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../img/desarrollo-ingenieria.png */ "./resources/img/desarrollo-ingenieria.png");
+/* harmony import */ var _img_marketing_png__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../img/marketing.png */ "./resources/img/marketing.png");
+/* harmony import */ var _img_english_png__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../../img/english.png */ "./resources/img/english.png");
+/* harmony import */ var _img_diseno_ux_png__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../../img/diseno-ux.png */ "./resources/img/diseno-ux.png");
+/* harmony import */ var _img_finanzas_png__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../../img/finanzas.png */ "./resources/img/finanzas.png");
+/* harmony import */ var _img_videogame_png__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../../img/videogame.png */ "./resources/img/videogame.png");
+/* harmony import */ var _img_estilo_de_vida_png__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ../../img/estilo-de-vida.png */ "./resources/img/estilo-de-vida.png");
+/* harmony import */ var _img_liderazgo_png__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ../../img/liderazgo.png */ "./resources/img/liderazgo.png");
+
+
+
+
+
+
+
+
 
 
 var _hoisted_1 = {
@@ -43,7 +59,9 @@ var _hoisted_6 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementV
 /* HOISTED */
 );
 
-var _hoisted_7 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("section", {
+var _hoisted_7 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<section class=\"categorias\"><div class=\"container py-5\"><div><h2 class=\"h1-responsive font-weight-bold text-center mb-3\"> Categorías </h2><!--Section description--><p class=\"text-center w-responsive mx-auto mb-5\"> Aprende lo que necesitas para desarrollar tu perfil profesional </p><div class=\"row\"><div class=\"col-xl-3 col-md-6 mb-4\"><div class=\"card\"><!-- Card body --><div class=\"card-body p-3\"><div class=\"d-flex align-items-center\"><div><img src=\"" + _img_desarrollo_ingenieria_png__WEBPACK_IMPORTED_MODULE_2__["default"] + "\" class=\"img-fluid w-75 text-center\"></div><div class=\"ms-3\"><span class=\"h6 font-weight-bold mb-0\">Desarrollo e Ingenería</span><h6 class=\"card-title text-capitalize text-muted text-sm mb-0\">90 Cursos</h6></div></div></div></div></div><div class=\"col-xl-3 col-md-6 mb-4\"><div class=\"card\"><!-- Card body --><div class=\"card-body p-3\"><div class=\"d-flex align-items-center\"><div><img src=\"" + _img_marketing_png__WEBPACK_IMPORTED_MODULE_3__["default"] + "\" class=\"img-fluid w-75 text-center\"></div><div class=\"ms-3\"><span class=\"h6 font-weight-bold mb-0\">Marketing</span><h6 class=\"card-title text-capitalize text-muted text-sm mb-0\">108 Cursos</h6></div></div></div></div></div><div class=\"col-xl-3 col-md-6 mb-4\"><div class=\"card\"><!-- Card body --><div class=\"card-body p-3\"><div class=\"d-flex align-items-center\"><div><img src=\"" + _img_english_png__WEBPACK_IMPORTED_MODULE_4__["default"] + "\" class=\"img-fluid w-75 text-center\"></div><div class=\"ms-3\"><span class=\"h6 font-weight-bold mb-0\">Inglés</span><h6 class=\"card-title text-capitalize text-muted text-sm mb-0\">55 Cursos</h6></div></div></div></div></div><div class=\"col-xl-3 col-md-6 mb-4\"><div class=\"card\"><!-- Card body --><div class=\"card-body p-3\"><div class=\"d-flex align-items-center\"><div><img src=\"" + _img_diseno_ux_png__WEBPACK_IMPORTED_MODULE_5__["default"] + "\" class=\"img-fluid w-75 text-center\"></div><div class=\"ms-3\"><span class=\"h6 font-weight-bold mb-0\">Diseño y UX</span><h6 class=\"card-title text-capitalize text-muted text-sm mb-0\">15 Cursos</h6></div></div></div></div></div></div><div class=\"row\"><div class=\"col-xl-3 col-md-6 mb-4\"><div class=\"card\"><!-- Card body --><div class=\"card-body p-3\"><div class=\"d-flex align-items-center\"><div><img src=\"" + _img_finanzas_png__WEBPACK_IMPORTED_MODULE_6__["default"] + "\" class=\"img-fluid w-75 text-center\"></div><div class=\"ms-3\"><span class=\"h6 font-weight-bold mb-0\">Finanzas e Inversiones</span><h6 class=\"card-title text-capitalize text-muted text-sm mb-0\">90 Cursos</h6></div></div></div></div></div><div class=\"col-xl-3 col-md-6 mb-4\"><div class=\"card\"><!-- Card body --><div class=\"card-body\"><div class=\"d-flex align-items-center\"><div><img src=\"" + _img_videogame_png__WEBPACK_IMPORTED_MODULE_7__["default"] + "\" class=\"img-fluid w-75 text-center\"></div><div class=\"ms-3\"><span class=\"h6 font-weight-bold mb-0\">Videojuegos</span><h6 class=\"card-title text-capitalize text-muted text-sm mb-0\">20 Cursos</h6></div></div></div></div></div><div class=\"col-xl-3 col-md-6 mb-4\"><div class=\"card\"><!-- Card body --><div class=\"card-body p-3\"><div class=\"d-flex align-items-center\"><div><img src=\"" + _img_estilo_de_vida_png__WEBPACK_IMPORTED_MODULE_8__["default"] + "\" class=\"img-fluid w-75 text-center\"></div><div class=\"ms-3\"><span class=\"h6 font-weight-bold mb-0\">Estilo de vida</span><h6 class=\"card-title text-capitalize text-muted text-sm mb-0\">89 Cursos</h6></div></div></div></div></div><div class=\"col-xl-3 col-md-6 mb-4\"><div class=\"card\"><!-- Card body --><div class=\"card-body p-3\"><div class=\"d-flex align-items-center\"><div><img src=\"" + _img_liderazgo_png__WEBPACK_IMPORTED_MODULE_9__["default"] + "\" class=\"img-fluid w-75 text-center\"></div><div class=\"ms-3\"><span class=\"h6 font-weight-bold mb-0\">Liderazgo</span><h6 class=\"card-title text-capitalize text-muted text-sm mb-0\">CEO</h6></div></div></div></div></div></div></div></div></section>", 1);
+
+var _hoisted_8 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("section", {
   "class": "contact"
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
   "class": "container align-items-center my-4"
@@ -159,7 +177,7 @@ function render(_ctx, _cache) {
     _: 1
     /* STABLE */
 
-  })])]), _hoisted_6])])]), _hoisted_7], 64
+  })])]), _hoisted_6])])]), _hoisted_7, _hoisted_8], 64
   /* STABLE_FRAGMENT */
   );
 }
@@ -275,6 +293,118 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("/images/bg.png?9fc2288778bf997f5c00bf670cfaf6b2");
+
+/***/ }),
+
+/***/ "./resources/img/desarrollo-ingenieria.png":
+/*!*************************************************!*\
+  !*** ./resources/img/desarrollo-ingenieria.png ***!
+  \*************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("/images/desarrollo-ingenieria.png?7abe8a92732508697f3f65f412358721");
+
+/***/ }),
+
+/***/ "./resources/img/diseno-ux.png":
+/*!*************************************!*\
+  !*** ./resources/img/diseno-ux.png ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("/images/diseno-ux.png?0d2027dd25a32e5da8fe9741b1a6e2f1");
+
+/***/ }),
+
+/***/ "./resources/img/english.png":
+/*!***********************************!*\
+  !*** ./resources/img/english.png ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("/images/english.png?aa85b1eee3159a259ab803aed75e0c42");
+
+/***/ }),
+
+/***/ "./resources/img/estilo-de-vida.png":
+/*!******************************************!*\
+  !*** ./resources/img/estilo-de-vida.png ***!
+  \******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("/images/estilo-de-vida.png?58cfad79967e00709f949f4315e35dab");
+
+/***/ }),
+
+/***/ "./resources/img/finanzas.png":
+/*!************************************!*\
+  !*** ./resources/img/finanzas.png ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("/images/finanzas.png?391bb4b967171a7b61efbfed31539bc7");
+
+/***/ }),
+
+/***/ "./resources/img/liderazgo.png":
+/*!*************************************!*\
+  !*** ./resources/img/liderazgo.png ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("/images/liderazgo.png?34efe75fbf95607cd2a9768eac381bd6");
+
+/***/ }),
+
+/***/ "./resources/img/marketing.png":
+/*!*************************************!*\
+  !*** ./resources/img/marketing.png ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("/images/marketing.png?1f58ddaa6df1ae45341adc2a0fbdc2f1");
+
+/***/ }),
+
+/***/ "./resources/img/videogame.png":
+/*!*************************************!*\
+  !*** ./resources/img/videogame.png ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("/images/videogame.png?478b5f3452dc35f887748ea2b8d266c6");
 
 /***/ }),
 
@@ -597,14 +727,14 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var _Home_vue_vue_type_template_id_f2b6376c__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Home.vue?vue&type=template&id=f2b6376c */ "./resources/js/components/Home.vue?vue&type=template&id=f2b6376c");
 /* harmony import */ var _Home_vue_vue_type_style_index_0_id_f2b6376c_lang_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Home.vue?vue&type=style&index=0&id=f2b6376c&lang=css */ "./resources/js/components/Home.vue?vue&type=style&index=0&id=f2b6376c&lang=css");
-/* harmony import */ var D_Laragon_www_Learnify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
+/* harmony import */ var C_laragon_www_learnify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
 
 const script = {}
 
 ;
 
 
-const __exports__ = /*#__PURE__*/(0,D_Laragon_www_Learnify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__["default"])(script, [['render',_Home_vue_vue_type_template_id_f2b6376c__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/components/Home.vue"]])
+const __exports__ = /*#__PURE__*/(0,C_laragon_www_learnify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__["default"])(script, [['render',_Home_vue_vue_type_template_id_f2b6376c__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/components/Home.vue"]])
 /* hot reload */
 if (false) {}
 
--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -41,7 +41,151 @@
       </div>
     </div>
   </section>
-  
+  <section class="categorias">
+    <div class="container py-5">
+      <div>
+        <h2 class="h1-responsive font-weight-bold text-center mb-3">
+          Categorías
+        </h2>
+        <!--Section description-->
+        <p class="text-center w-responsive mx-auto mb-5">
+          Aprende lo que necesitas para desarrollar tu perfil profesional
+        </p>
+        <div class="row">
+          <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card">
+              <!-- Card body -->
+              <div class="card-body p-3">
+                <div class="d-flex align-items-center">
+                  <div>
+                    <img src="../../img/desarrollo-ingenieria.png" class="img-fluid w-75 text-center">
+                  </div>
+                  <div class="ms-3">
+                    <span class="h6 font-weight-bold mb-0">Desarrollo e Ingenería</span>
+                    <h6 class="card-title text-capitalize text-muted text-sm mb-0">90 Cursos</h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card">
+            <!-- Card body -->
+            <div class="card-body p-3">
+              <div class="d-flex align-items-center">
+                <div>
+                  <img src="../../img/marketing.png" class="img-fluid w-75 text-center">
+                </div>
+                <div class="ms-3">
+                  <span class="h6 font-weight-bold mb-0">Marketing</span>
+                  <h6 class="card-title text-capitalize text-muted text-sm mb-0">108 Cursos</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+          <div class="card">
+            <!-- Card body -->
+            <div class="card-body p-3">
+              <div class="d-flex align-items-center">
+                <div>
+                  <img src="../../img/english.png" class="img-fluid w-75 text-center">
+                </div>
+                <div class="ms-3">
+                  <span class="h6 font-weight-bold mb-0">Inglés</span>
+                  <h6 class="card-title text-capitalize text-muted text-sm mb-0">55 Cursos</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+          <div class="card">
+            <!-- Card body -->
+            <div class="card-body p-3">
+              <div class="d-flex align-items-center">
+                <div>
+                  <img src="../../img/diseno-ux.png" class="img-fluid w-75 text-center">
+                </div>
+                <div class="ms-3">
+                  <span class="h6 font-weight-bold mb-0">Diseño y UX</span>
+                  <h6 class="card-title text-capitalize text-muted text-sm mb-0">15 Cursos</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xl-3 col-md-6 mb-4">
+          <div class="card">
+            <!-- Card body -->
+            <div class="card-body p-3">
+              <div class="d-flex align-items-center">
+                <div>
+                  <img src="../../img/finanzas.png" class="img-fluid w-75 text-center">
+                </div>
+                <div class="ms-3">
+                  <span class="h6 font-weight-bold mb-0">Finanzas e Inversiones</span>
+                  <h6 class="card-title text-capitalize text-muted text-sm mb-0">90 Cursos</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+          <div class="card">
+            <!-- Card body -->
+            <div class="card-body">
+              <div class="d-flex align-items-center">
+                <div>
+                  <img src="../../img/videogame.png" class="img-fluid w-75 text-center">
+                </div>
+                <div class="ms-3">
+                  <span class="h6 font-weight-bold mb-0">Videojuegos</span>
+                  <h6 class="card-title text-capitalize text-muted text-sm mb-0">20 Cursos</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+          <div class="card">
+            <!-- Card body -->
+            <div class="card-body p-3">
+              <div class="d-flex align-items-center">
+                <div>
+                  <img src="../../img/estilo-de-vida.png" class="img-fluid w-75 text-center">
+                </div>
+                <div class="ms-3">
+                  <span class="h6 font-weight-bold mb-0">Estilo de vida</span>
+                  <h6 class="card-title text-capitalize text-muted text-sm mb-0">89 Cursos</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+          <div class="card">
+            <!-- Card body -->
+            <div class="card-body p-3">
+              <div class="d-flex align-items-center">
+                <div>
+                  <img src="../../img/liderazgo.png" class="img-fluid w-75 text-center">
+                </div>
+                <div class="ms-3">
+                  <span class="h6 font-weight-bold mb-0">Liderazgo</span>
+                  <h6 class="card-title text-capitalize text-muted text-sm mb-0">CEO</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+  </section>
   <section class="contact">
     <div class="container align-items-center my-4">
       <section class="mb-4">
