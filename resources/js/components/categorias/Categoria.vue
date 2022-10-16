10b293557087ddf5b75e87464bbd1d3d17da4441
Jalil M. López <jalilmanuel25@gmail.com>
2022-10-16 13:38:29 -0500

    Se hace componente de categoria

--- resources/js/components/categorias/Categoria.vue
+++ resources/js/components/categorias/Categoria.vue
@@ -3,14 +3,16 @@
       <div class="container">
         <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="#" class="text-decoration-none" style="color:#184E77">Inicio</a></li>
-                <li class="breadcrumb-item" aria-current="page"><a href="" class="text-decoration-none" style="color:#184E77">Categorías</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Desarrollo e Ingeniería</li>
+                <li class="breadcrumb-item"><router-link to="/" class="text-decoration-none" style="color:#184E77">Inicio</router-link></li>
+                <li class="breadcrumb-item" aria-current="page"><router-link to="/categorias" class="text-decoration-none" style="color:#184E77">Categorías</router-link></li>
+                <li class="breadcrumb-item active" aria-current="page">
+                    <router-link :to="'/categoria/' + this.name" class="text-decoration-none" style="color:#184E77">{{ this.titulo }}</router-link>
+                </li>
             </ol>
         </nav>
         <div class="">
             <h2 class="h1-responsive font-weight-bold text-left mb-3 mt-4">
-                <b>Cursos de <spam style="color:#184E77">Desarrollo e Ingeniería</spam></b>
+                <b>Cursos de <spam style="color:#184E77">{{ this.titulo }}</spam></b>
             </h2>
             <div class="clearfix mb-5">
                 <p class="float-start fw-light" style="font-size:12px;">Resultados</p>
@@ -36,16 +38,45 @@
         </div>
       </div>
     </div>
-
-    <Footer />
 </template>
 
 <script>
-import Footer from '../home/Footer.vue';
+    import Footer from '../home/Footer.vue';
+    import categorias from './categorias';
+
     export default {
-    name: "Categoria",
-    components: { Footer }
-}
+        name: "Categoria",
+        data() {
+            return {
+                id: null,
+                name: "",
+                titulo: "",
+                descripcion: "",
+                cursos: null,
+                imagen: ""
+            }
+        },
+        components: { Footer },
+        mounted() {
+            this.cargarCategoriaData()
+        },
+        methods: {
+            cargarCategoriaData() {
+                categorias.map(categoria => {
+                    if(categoria.name == this.$route.params.categoria) {
+                        let { id, name, titulo, descripcion, cursos, imagen } = categoria;
+
+                        this.id = id;
+                        this.name = name;
+                        this.titulo = titulo;
+                        this.descripcion = descripcion;
+                        this.cursos = cursos;
+                        this.imagen = imagen;
+                    }
+                });
+            }
+        }
+    }
 </script>
 
 <style scoped>
