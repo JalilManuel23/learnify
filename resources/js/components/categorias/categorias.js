a754b4acab79e1cc5d4ff8a466ba7034a2f515fa
Jalil M. López <jalilmanuel25@gmail.com>
2022-10-16 13:39:19 -0500

    Se agrega categorias data

---
+++ resources/js/components/categorias/categorias.js
@@ -0,0 +1,68 @@
+const categorias = [
+    {
+        "id": 1,
+        "name": "ingenieria-desarrollo",
+        "titulo": "Ingenieria y Desarrollo",
+        "descripcion": "Cursos de ingeniería y desarrollo",
+        "cursos": 90,
+        "imagen": require("../../../img/desarrollo-ingenieria.png").default
+    },
+    {
+        "id": 2,
+        "name": "marketing",
+        "titulo": "Marketing",
+        "descripcion": "Cursos de marketing",
+        "cursos": 108,
+        "imagen": require("../../../img/marketing.png").default
+    },
+    {
+        "id": 3,
+        "name": "ingles",
+        "titulo": "Inglés",
+        "descripcion": "Cursos de inglés",
+        "cursos": 55,
+        "imagen": require("../../../img/english.png").default
+    },
+    {
+        "id": 4,
+        "name": "diseno-ux",
+        "titulo": "Diseño y UX",
+        "descripcion": "Cursos de de diseño",
+        "cursos": 15,
+        "imagen": require("../../../img/diseno-ux.png").default
+    },
+    {
+        "id": 5,
+        "name": "finanzas-inversiones",
+        "titulo": "Finanzas e Inversiones",
+        "descripcion": "Cursos de finanzas",
+        "cursos": 90,
+        "imagen": require("../../../img/finanzas.png").default
+    },
+    {
+        "id": 6,
+        "name": "videojuegos",
+        "titulo": "Vídeojuegos",
+        "descripcion": "Cursos de videojuegos",
+        "cursos": 20,
+        "imagen": require("../../../img/videogame.png").default
+    },
+    {
+        "id": 7,
+        "name": "estilo-vida",
+        "titulo": "Estilo de vida",
+        "descripcion": "Cursos de estilo de vida",
+        "cursos": 89,
+        "imagen": require("../../../img/estilo-de-vida.png").default
+    },
+    {
+        "id": 8,
+        "name": "liderazgo",
+        "titulo": "Liderazgo",
+        "descripcion": "Cursos de liderazgo",
+        "cursos": 35,
+        "imagen": require("../../../img/liderazgo.png").default
+    },
+]
+
+export default categorias;
