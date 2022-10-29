83506206c13cffa20e2478c6286798a3570df58f
Jalil <jalilmanuel25@gmail.com>
2022-10-29 10:18:47 -0500

    Se arreglaron detalles de estilos en el reproductor de video

--- resources/js/components/VideoPlayer.vue
+++ resources/js/components/VideoPlayer.vue
@@ -64,5 +64,73 @@
 </script>
 
 <template>
-    <div ref="player" style="width: 600px; height: 400px;"></div>
+    <div class="row">    
+        <div class="col-md-8">
+            <div ref="player" class="w-100 p-0 player"></div>
+            <h3>Título del vídeo</h3>
+        </div>
+        <div class="col-md-4 mt-5">
+            <h6 class="text-bold">Contenido del curso</h6>
+            <ul>
+                <li class="enlace-video">
+                    <a href="#">1. Introducción</a> 
+                    <div class="d-flex align-items-center">
+                            <i class="fa fa-play-circle mr-2" aria-hidden="true"></i> 
+                            <p style="margin-left: 3px;">5 min</p>
+                    </div>
+                </li>
+                <li class="enlace-video">
+                    <a href="#">2. Vídeo 1</a>
+                    <div class="d-flex align-items-center">
+                        <i class="fa fa-play-circle mr-2" aria-hidden="true"></i> 
+                        <p style="margin-left: 3px;">5 min</p>
+                   </div>
+                </li>
+                <li class="enlace-video">
+                    <a href="#">3. Vídeo 2</a>
+                    <div class="d-flex align-items-center">
+                        <i class="fa fa-play-circle mr-2" aria-hidden="true"></i> 
+                        <p style="margin-left: 3px;">5 min</p>
+                    </div>
+                </li>
+                <li class="enlace-video">
+                    <a href="#">4. Vídeo 2</a>
+                    <div class="d-flex align-items-center">
+                        <i class="fa fa-play-circle mr-2" aria-hidden="true"></i> 
+                        <p style="margin-left: 3px;">5 min</p>
+                    </div>
+                </li>
+                <li class="enlace-video">
+                    <a href="#">5. Vídeo 4</a>
+                    <div class="d-flex align-items-center">
+                        <i class="fa fa-play-circle mr-2" aria-hidden="true"></i> 
+                        <p style="margin-left: 3px;">5 min</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
+
+<style>
+.player {
+    height: 80vh;
+}
+
+ul {
+    list-style: none;
+}
+
+.enlace-video a {
+    text-decoration: none;
+    color: black;
+}
+
+.enlace-video {
+    padding: 20px;
+    border-bottom:1px solid rgb(197, 194, 194);
+    display: flex;
+    flex-direction: column;
+}
+
+</style>
