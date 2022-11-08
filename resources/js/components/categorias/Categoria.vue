10bc049406925adf20ebbc6b7ccb2b836f4bf998
iammarieh <mhf.08@hotmail.com>
2022-11-08 11:15:50 -0600

    Estilos en breadcrumb

--- resources/js/components/categorias/Categoria.vue
+++ resources/js/components/categorias/Categoria.vue
@@ -3,10 +3,10 @@
       <div class="container">
         <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><router-link to="/" class="text-decoration-none" style="color:#184E77">Inicio</router-link></li>
-                <li class="breadcrumb-item" aria-current="page"><router-link to="/categorias" class="text-decoration-none" style="color:#184E77">Categorías</router-link></li>
+                <li class="breadcrumb-item"><router-link to="/" class="text-decoration-none" style="color:#808080;">Inicio</router-link></li>
+                <li class="breadcrumb-item" aria-current="page"><router-link to="/categorias" class="text-decoration-none" style="color:#2B3856">Categorías</router-link></li>
                 <li class="breadcrumb-item active" aria-current="page">
-                    <router-link :to="'/categoria/' + this.name" class="text-decoration-none" style="color:#184E77">{{ this.titulo }}</router-link>
+                    <router-link :to="'/categoria/' + this.name" class="text-decoration-none" style="color:#184E77"><strong>{{ this.titulo }}</strong></router-link>
                 </li>
             </ol>
         </nav>
